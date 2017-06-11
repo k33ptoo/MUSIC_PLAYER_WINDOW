33c9b17101540a0a9becc93750fca3980cd0c923
Amos Chepchieng <akchieng@gmail.com>
2017-06-11 21:35:51 +0300

    Create Main.java
    That should  work.

--- Sangeet/src/sample/Main.java
+++ Sangeet/src/sample/Main.java
@@ -15,11 +15,30 @@ import javafx.stage.WindowEvent;
 import java.awt.event.ActionEvent;
 
 public class Main extends Application {
-
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
+        
+        //Init Grab
+        
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
